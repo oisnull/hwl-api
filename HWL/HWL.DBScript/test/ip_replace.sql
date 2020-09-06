2672c52f71ff0b489a644877740962915f124a0d
o is null <1577592799@qq.com>
2020-09-06 13:59:10 +0800

    Added ip replace sql scripe for local test

---
+++ HWL/HWL.DBScript/test/ip_replace.sql
@@ -0,0 +1,20 @@
+﻿select * from t_user
+where head_image like '%2.210:8033%'
+
+update t_user set head_image=REPLACE(head_image,'2.210:8033','2.223:8033')
+where head_image like '%2.210:8033%'
+update t_user set circle_back_image=REPLACE(circle_back_image,'2.210:8033','2.223:8033')
+where circle_back_image like '%2.210:8033%'
+
+
+
+select * from t_circle
+where image_urls like '%2.210:8033%'
+
+select * from t_near_circle
+where image_urls like '%2.210:8033%'
+
+update t_circle set image_urls=REPLACE(image_urls,'2.210:8033','2.223:8033')
+where image_urls like '%2.210:8033%'
+update t_near_circle set image_urls=REPLACE(image_urls,'2.210:8033','2.223:8033')
+where image_urls like '%2.210:8033%'
