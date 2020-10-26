8f94e57d39b0cc85cae1f1e052fef1d27e6d97ef
Yang Li (Beyondsoft Corporation) <v-yali3@microsoft.com>
2020-10-26 16:20:43 +0800

    db backup list in mgr code

---
+++ HWL/HWL.Manage/Controllers/SystemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HWL.Manage.Controllers
+{
+    public class SystemController : BaseController
+    {
+        public ActionResult DBBackup()
+        {
+            return View();
+        }
+    }
+}
